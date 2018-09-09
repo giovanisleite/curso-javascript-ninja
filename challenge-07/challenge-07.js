d2f1e1370f241c1c3fe1f01b4a6685ed97ad2a24
Giovani <sousa_giovani@hotmail.com>
2018-09-09 17:39:42 -0300

    Add challenge 07 fixes

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -16,14 +16,14 @@ function addItem(item) {
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-addItem([34, null, undefined]);
+console.log(addItem([34, null, undefined]));
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
-console.log(arr[arr.length - 1][1]);
+console.log(`O segundo elemento do segundo array é ${arr[arr.length - 1][1]}.`);
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
@@ -42,7 +42,7 @@ Utilizando a estrutura de repetição `while`, mostre no console todos os númer
 pares entre 10 e 20, inclusive esses 2.
 */
 console.log("Números pares entre 10 e 20:");
-i = 10;
+var i = 10;
 while (i <= 20) {
   if (i % 2 == 0) console.log(i);
   i++;
@@ -52,7 +52,7 @@ while (i <= 20) {
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
 console.log("Números ímpares entre 10 e 20:");
-i = 10;
+var i = 10;
 while (i <= 20) {
   if (i % 2 == 1) console.log(i);
   i++;
